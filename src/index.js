a46c754de7c9fed985c4e6ceaa76fab3c9197bcb
Raj Bhinde <68704186+irajbhinde@users.noreply.github.com>
2022-08-06 23:06:23 +0530

    feat : mockbee integration

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { QuizProvider } from "./components/context/quiz-context";
+import { makeServer } from "./server";
+import { AuthProvider } from "./components/index";
 
+makeServer();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <QuizProvider>
-        <App />
-      </QuizProvider>
+      <AuthProvider>
+        <QuizProvider>
+          <App />
+        </QuizProvider>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
@@ -20,4 +24,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/server.js
@@ -0,0 +1,35 @@
+import { Server, Model, RestSerializer } from "miragejs";
+import {
+  loginHandler,
+  signupHandler,
+} from "./backend/controllers/AuthController";
+import { users } from "./backend/db/users";
+
+export function makeServer({ environment = "development" } = {}) {
+  let server = new Server({
+    serializers: {
+      application: RestSerializer,
+    },
+    environment,
+    models: {
+      user: Model,
+    },
+
+    // Runs on the start of the server
+    seeds(server) {
+      users.forEach((item) =>
+        server.create("user", {
+          ...item,
+        })
+      );
+    },
+
+    routes() {
+      this.namespace = "api";
+      // auth routes (public)
+      this.post("/auth/signup", signupHandler.bind(this));
+      this.post("/auth/login", loginHandler.bind(this));
+    },
+  });
+  return server;
+}
