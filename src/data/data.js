327c8e8571492c3303d2013196f68aaeeffd3bbb
Raj Bhinde <68704186+irajbhinde@users.noreply.github.com>
2022-08-22 14:13:53 +0530

    feat : added 2 categories to the quiz app

--- src/data/data.js
+++ src/data/data.js
@@ -1,4 +1,4 @@
-import { neog, friends } from "../assets";
+import { neog, friends, himym, suits } from "../assets";
 
 export const categories = [
   {
@@ -13,5 +13,19 @@ export const categories = [
     description:
       "Think you're an expert on all things Central Perk? Take this Friends quiz to prove whether or not you're a true fan.",
     image: friends,
+  },
+  {
+    categoryName: "How I Met Your Mother",
+    categoryNumber: 2,
+    description:
+      "This Is The Hardest How I Met Your Mother Knowledge Test You'll Ever Take",
+    image: himym,
+  },
+  {
+    categoryName: "Suits",
+    categoryNumber: 3,
+    description:
+      "How much do you know about USA's Suits? Take our quiz and find out now!",
+    image: suits,
   },
 ];
